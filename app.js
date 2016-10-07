d1e07f0a73bd1cce0fb4782adbaa3c642a81f688
f3r10 <fernando.ledesma@epn.edu.ec>
2016-10-07 16:37:53 -0500

    Update to bank contract

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var erisC = require('eris-contracts');
 // and find the docker machine name you are using (usually default or eris).
 // for example, if the URL returned by docker-machine is tcp://192.168.99.100:2376
 // then your erisdbURL should be http://192.168.99.100:1337/rpc
-var erisdbURL = "http://localhost:1337/rpc";
+var erisdbURL = "http://172.141.20.7:1337/rpc";
 
 // get the abi and deployed data squared away
 var contractData = require('./epm.json');
@@ -19,42 +19,42 @@ var idisAbi = JSON.parse(fs.readFileSync("./abi/" + idisContractAddress));
 // properly instantiate the contract objects manager using the erisdb URL
 // and the account data (which is a temporary hack)
 var accountData = require('./accounts.json');
-var contractsManager = erisC.newContractManagerDev(erisdbURL, accountData.simplechain_full_000);
+var contractsManager = erisC.newContractManagerDev(erisdbURL, accountData.mibuspaychain_full_000);
 
 // properly instantiate the contract objects using the abi and address
 var idisContract = contractsManager.newContractFactory(idisAbi).at(idisContractAddress);
 
 // display the current value of idi's contract by calling
 // the `get` function of idi's contract
-function getValue(callback) {
-  idisContract.get(function(error, result){
-    if (error) { throw error }
-    console.log("Idi's number is:\t\t\t" + result.toNumber());
-    callback();
-  });
-}
+// function endow(callback) {
+//   idisContract.endow(function(error, result){
+//     if (error) { throw error }
+//     console.log("Idi's number is:\t\t\t" + result.toNumber());
+//     callback();
+//   });
+// }
 
 // prompt the user to change the value of idi's contract
-function changeValue() {
-  prompt.message = "What number should Idi make it?";
+function createAccount() {
+  prompt.message = "Crear cuenta:";
   prompt.delimiter = "\t";
   prompt.start();
   prompt.get(['value'], function (error, result) {
     if (error) { throw error }
-    setValue(result.value)
+    registerNewAccount(result.value.toString)
   });
 }
 
 // using eris-contracts call the `set` function of idi's
 // contract using the value which was recieved from the
-// changeValue prompt
-function setValue(value) {
-  idisContract.set(value, function(error, result){
+// createAccount prompt
+function registerNewAccount(value) {
+  idisContract.registerNewAccount(value, function(error, result){
     if (error) { throw error }
-    getValue(function(){});
+    //endow(function(){});
   });
 }
 
 // run
-getValue(changeValue);
+getValue(createAccount);
 
